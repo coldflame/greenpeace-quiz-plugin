e0c6206689f6284596636f131dc30616a14f4285
umair <theumairshahid@gmail.com>
2017-06-07 17:22:49 +0500

    plugin implemented and explained

--- README.md
+++ README.md
@@ -2,6 +2,47 @@
 A jQuery plugin on top of twitter bootstrap for creating dynamic quizzes.
 
 
+
 ## Demo And Usage
 
 See `index.html` for demo and suggested HTML structure (the element class names are the important part).
+
+## Available Options
+
+To initialize your quiz:
+
+    $(function () {
+        $('.quizme').quiz({
+            questions = []
+            // options can be added
+        });
+    });
+
+
+## Base Config Options
+
+See `Index.html`
+
+    var questionsJSON = [ {"type":"text", 
+                          "questionText": "Pick an animal",
+                          "options" : []
+                         },
+                         {"type":"image", 
+                          "questionText": "Pick an animal",
+                          "options" : []
+                         } 
+                        ]
+    // For image type currently support multiple of 3 options.
+    var optionsJSON    = [ {"label":"","category":"Greenpeace","imgSrc":"https://img.buzzfeed.com/buzzfeed-static/static/2014-10/16/9/enhanced/webdr09/enhanced-buzz-768-1413466971-1.jpg?downsize=715:*&output-format=auto&output-quality=auto"},
+                                              {"label":"","category":"Greenpeace","imgSrc":"https://img.buzzfeed.com/buzzfeed-static/static/2014-10/16/9/enhanced/webdr11/enhanced-buzz-1858-1413467065-16.jpg?downsize=715:*&output-format=auto&output-quality=auto"},
+                                              {"label":"","category":"Greenpeace","imgSrc":"https://img.buzzfeed.com/buzzfeed-static/static/2014-10/16/9/enhanced/webdr08/enhanced-buzz-18695-1413467110-38.jpg?downsize=715:*&output-format=auto&output-quality=auto"}
+                           ]
+    // For text type
+    var optionsJSON    = [  {"text":"Trustworthy and steadfast"},
+                                              {"text":"Spontaneous and easy going"},
+                                              {"text":"Tough and stoic"}
+                           ]                     
+                           
+
+
+Created by [Umair Shahid](http://github.com/coldflame) 
--- js/quiz.js
+++ js/quiz.js
@@ -12,31 +12,116 @@
     }
 
     Quiz.prototype = {
-
-        constructor: Quiz
-
-        , 
+        constructor: Quiz, 
         init: function (element, options) {           
-            this.$element = $(element)
-            this.options = this.getOptions(options)	
-            var template = $(this.options.template);
-            if(template != '')
-            {
-                template.find(".bDiv").css("height",this.options.gridHeight)
-                this.$element.find(this.options.rightCol).append(template)
+            this.$element = $(element);
+            this.options = this.getOptions(options);	            
+            this.questions = this.options.questions;
+            this.currentQuestionNo = 0;
+            this.questionBody = this.$element.find(".qestion-body");                                          
+            this.nextButton = this.$element.find(".btn-next");
+            this.backButton = this.$element.find(".btn-back");
+            this.currentQuestion = this.questions[this.currentQuestionNo];
+            this.drawQuestion(this.currentQuestion);
+            
+            this.nextButton.on("click",$.proxy(this.nextQuestion,this));
+            this.backButton.on("click",$.proxy(this.prevQuestion,this));  
+        },
+        nextQuestion:function(){
+            if(!this.nextButton.hasClass("disabled")){
+                this.currentQuestionNo = this.currentQuestionNo + 1;
+                this.currentQuestion = this.questions[this.currentQuestionNo];
+                this.drawQuestion(this.currentQuestion);
+                
+                if(this.currentQuestionNo==this.questions.length-1){
+                    this.nextButton.addClass("disabled");
+                }
+                if(this.currentQuestionNo>0){
+                    this.backButton.removeClass("disabled");
+                }
             }
-            this.mappingInit()                  
+        },
+        prevQuestion:function(){
+            if(!this.backButton.hasClass("disabled")){
+                this.currentQuestionNo = this.currentQuestionNo - 1;
+                this.currentQuestion = this.questions[this.currentQuestionNo];
+                this.drawQuestion(this.currentQuestion);
+                if(this.currentQuestionNo==0){
+                    this.backButton.addClass("disabled");
+                }                
+                this.nextButton.removeClass("disabled");                
+            }    
         }
         ,
-        mappingInit:function(){
-	 
-        }  
-        ,
-        moveAll:function(){
-            this.$element.find(".col1 .bmsgrid tr .move-row").click()
+        drawQuestion:function(q){
+            if(q){                
+                this.$element.find(".page-header h3").text((this.currentQuestionNo + 1)+"- "+q.questionText);
+                if(q.type=="image"){
+                    this.drawImageType(q);
+                }
+                else if(q.type=="text"){
+                    this.drawTextType(q);
+                }               
+            }
         },
-        getCol2:function(){
-            return this.options.rightCol;
+        drawImageType:function(q){
+            var template = $('<div class="image-type"></div>')
+            this.questionBody.hide().html(template).fadeIn();            
+            var rowContainer = null;
+            var that = this;
+             $.each(q.options,function(i,opt){
+                 if(i%3==0){
+                     rowContainer = that.addImageRow(template);
+                 }
+                 var imgNode = that.addImageOption(opt);
+                 rowContainer.append(imgNode);
+                 imgNode.on("click",$.proxy(that.markSelected, that, i));  
+             });
+            
+        },
+        addImageRow:function(container){
+            var imageRow = $('<div class="row"></div>');
+            container.append(imageRow);
+            return imageRow;
+        },
+        addImageOption: function (opt){
+            return  $('<div class="col-sm-6 col-md-4">\n\
+                    <div class="thumbnail">  \n\
+                            <img src="'+opt.imgSrc+'" alt="">\n\
+                        <div class="caption">\n\
+                            <small>'+opt.category+'</small>\n\
+                            <h4>'+opt.label+'</h4>\n\
+                        </div>\n\
+                        <div class="select-mask"></div>\n\
+                        <button type="button" class="btn btn-success btn-lg select-button">Select</button>    \n\
+                    </div> \n\
+               </div>');
+            
+        }
+        ,
+        drawTextType:function(q){
+            var template = $('<div class="text-type"></div>');            
+            this.questionBody.hide().html(template).fadeIn(); 
+            var textContainer = $('<div class="list-group"></div>');
+            var that = this;
+            template.html(textContainer);
+            $.each(q.options,function(i,opt){          
+                var textNode = that.textNode(opt);
+                textContainer.append(textNode);
+                textNode.on("click",$.proxy(that.markSelected, that, i));  
+            });
+        },
+        textNode:function(opt){
+            return $('<a href="#" class="list-group-item"><input type="checkbox"> '+opt.text+'</a>');
+            
+        },
+        markSelected: function(index){
+            var nodeClass = ".thumbnail";
+            if(this.currentQuestion.type=="text"){
+                nodeClass = ".list-group-item";
+                this.questionBody.find(nodeClass+" input[type='checkbox']").prop("checked",false).eq(index).prop("checked",true);
+            }            
+            this.questionBody.find(nodeClass).removeClass("active").eq(index).addClass("active");            
         }
         , 
         getOptions: function (options) {
@@ -65,12 +150,9 @@
     $.fn.quiz.Constructor = Quiz
 
     $.fn.quiz.defaults = { 
-        template:'<div class="container"> </div>',                   
+        template:'',                   
         title:'Quiz Title',        
-        leftCol:".col1 .leftcol",
-        moveSelector:".move-row",        
-        loadTarget: '',
-        copyTarget: ''
+        questions:[],
     }
 
 }(window.jQuery);
